984c6308b1c3d04dcbf7866d9d738f5dc2414d40
Mishael2004 <mishabykov228@gmail.com>
2022-03-16 18:51:29 +0200

    Добавляет файлы сайта

--- js/script.js
+++ js/script.js
@@ -10,46 +10,12 @@ let body = document.querySelector('body');
 })
 
 
-
-let nav = document.querySelector('.nav');
-
-    nav.addEventListener('click', function(){
-    window.scrollTo(0, 800)
-})
-
-let cursor = document.querySelector('.cursor')
-let start = document.querySelector('.start');
-
-start.addEventListener('click', function(){
-    cursor.value = '0'
-    window.scrollTo(0, 0);
-    
-})
-
-let one = document.querySelector('.one');
-
-    one.addEventListener('click', function(){
-    cursor.value = '1'
-    window.scrollTo(0, 1000);
-    
-});
-
-
-let two = document.querySelector('.two');
-
-    two.addEventListener('click', function(){
-    cursor.value = '2'
-    window.scrollTo(0, 1850)
-})
-
-
-let three = document.querySelector('.three');
-   
-    three.addEventListener('click', function(){
-    cursor.value = '3'
-    window.scrollTo(0, 2750)
-})
-
+$(".scale").on("click","a", function (event) {
+    event.preventDefault();
+    var id  = $(this).attr('href'),
+        top = $(id).offset().top;
+    $('body,html').animate({scrollTop: top}, 1000);
+  })
 
 
 
